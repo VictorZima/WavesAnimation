59c3ff5999473f25936e1929ebb5352b95e5773f
VictorZima <victorzima@icloud.com>
2022-08-12 02:29:15 +0300

    Initial Commit

---
+++ SeaAndWaves/SeaAndWavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaAndWavesApp.swift
+//  SeaAndWaves
+//
+//  Created by VictorZima on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SeaAndWavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
